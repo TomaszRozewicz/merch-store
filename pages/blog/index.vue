6bb31f13c1ddb005c4957234f8d6fdc561b39677
Tomek Różewicz <tomek.rozewicz@snow.dog>
2020-06-19 14:15:56 +0200

    Add blog list

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,9 +1,23 @@
+<template>
+  <div>
+    <a
+      v-for="(item, key) in blogPosts"
+      :key="key"
+      :href="`/blog/${item.slug}`"
+    >
+      {{ item.title }}
+    </a>
+  </div>
+</template>
 <script>
 export default {
   computed: {
     blogPosts () {
       return this.$store.state.blogPosts
     }
+  },
+  mounted () {
+    console.log(this.blogPosts)
   }
 }
 </script>
